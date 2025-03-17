141b79228ccd56cdf24cea84b75b691eec8bb758
kenny2575 <rus.nigmat@gmail.com>
2025-03-17 18:51:35 +0700

    Запуск по расписанию

---
+++ src/main/java/com/example/t1academylimits/service/RefreshLimitsService.java
@@ -0,0 +1,17 @@
+package com.example.t1academylimits.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshLimitsService {
+
+
+    //@Transactional(isolation = Isolation.REPEATABLE_READ)
+    public void refreshLimits(BigDecimal amount) {
+
+    }
+
+}
